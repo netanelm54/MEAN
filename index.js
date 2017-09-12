c5fa423abdfd9abb44cba9549bc9db7247cc8135
נתנאל מורלי <netanelm54@gmail.com>
2017-09-12 13:51:09 +0300

    setup client

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const path = require('path');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
@@ -11,8 +12,10 @@ mongoose.connect(config.uri, (err) => {
         console.log('Connected to datebase: ' + config.db);
 });
 
+app.use(express.static(__dirname + '/client/dist/'));
+
 app.get('*', (req, res) => {
-    res.send('hello my friend');
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
 app.listen(8080, () => {
