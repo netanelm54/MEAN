fe36f838c418cce906c2dd7e72d5a66c9cd84b75
נתנאל מורלי <netanelm54@gmail.com>
2017-09-13 19:01:12 +0300

    nav bar location

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRouting } from './app.routing'
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './pages/navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
